52b93db2156f7c96a3e6e5f7a02dc593ca73775d
rows <marceloxpaduan@gmail.com>
2022-03-28 21:22:24 -0300

    aiuuu

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export const portf = [{
     id: 1,
-    img: "https://img.ifunny.co/images/06ad80453f54d5658c0a4f8a7db873e9946068d99dd077c8a514c6512d77b2aa_1.jpg",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    titulo: "Titu1"
+    img: "https://veja.abril.com.br/wp-content/uploads/2021/02/GettyImages-1229153769.jpg",
+    link: "https://www.tiktok.com/@isa.barduchi/video/7079941292599381253?is_copy_url=1&is_from_webapp=v1",
+    titulo: "App Spotify"
 },
 {
     id: 2,
-    img: "https://img.ifunny.co/images/7d4a703317e8b97bed33253456a8e1bcbe66b382227ac9c253f34496d995c7ed_1.jpg",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    titulo: "Titu2"
+    img: "http://www.sorocaba.sp.gov.br/wp-content/uploads/2015/08/logo_pms_ver_color-menor.png",
+    link: "https://www.tiktok.com/@isa.barduchi/video/7079941292599381253?is_copy_url=1&is_from_webapp=v1",
+    titulo: "Website Prefeitura de Sorocaba"
 },
 {
     id: 3,
-    img: "https://www.meme-arsenal.com/memes/6fa83d93253b5d6e5a1a1ce70e7b9ac8.jpg",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    titulo: "Titu3"
+    img: "https://content.paodeacucar.com/wp-content/uploads/2019/06/8-dicas-%C3%BAteis-2.jpg",
+    link: "https://www.tiktok.com/@isa.barduchi/video/7079941292599381253?is_copy_url=1&is_from_webapp=v1",
+    titulo: "Sistema de vendas de mercado local"
 },
 {
     id: 4,
-    img: "https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/cba8afd614992aa523912cc3cb8bb7f5352d2550014f3e9699c4eb88cd91f2fb_1.jpg",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    titulo: "Titu4"
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Bing_logo_%282016%29.svg/800px-Bing_logo_%282016%29.svg.png",
+    link: "https://www.tiktok.com/@isa.barduchi/video/7079941292599381253?is_copy_url=1&is_from_webapp=v1",
+    titulo: "App de busca de imagens"
 },
 {
     id: 5,
-    img: "https://i.pinimg.com/originals/e5/5d/d3/e55dd3754d2e5660edaaafd679170702.jpg",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    titulo: "Titu5"
+    img: "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fkilling-floor-2%2Fhome%2FEGS_KillingFloor2_TripwireInteractive_G1A_01-1920x1080-e42031811a8ed6dff58ef299bbb6601ec565b32b.jpg",
+    link: "https://www.tiktok.com/@isa.barduchi/video/7079941292599381253?is_copy_url=1&is_from_webapp=v1",
+    titulo: "Killing floor 2"
 },
 {
     id: 6,
-    img: "https://i.pinimg.com/474x/11/1d/8b/111d8bf7b6a290071bf6c9052379c5ff.jpg",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    titulo: "Titu6"
+    img: "https://cdn.forums.klei.com/monthly_2017_12/dont-starve-together.jpg.30f62ee49306e55accb8cff441e220ee.jpg",
+    link: "https://www.tiktok.com/@isa.barduchi/video/7079941292599381253?is_copy_url=1&is_from_webapp=v1",
+    titulo: "Dont Starve Together"
 },
 ]
